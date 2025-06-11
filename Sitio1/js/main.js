db9a91c10132a1fdcfdb189128afc1929b8fe3ec
FernandoE27 <josefer200427@gmail.com>
2025-06-10 21:32:37 -0600

    Ubicacion e informacion del contacto corregida

--- Sitio1/js/main.js
+++ Sitio1/js/main.js
@@ -88,3 +88,16 @@ document.addEventListener("keydown", (e) => {
         modal.style.display = "none";
     }
 });
+
+const menuToggle = document.querySelector('.menu-toggle');
+const menu = document.querySelector('.menu');
+
+menuToggle.addEventListener('click', () => {
+    menu.classList.toggle('active');
+});
+
+document.querySelectorAll('.menu a').forEach(link => {
+    link.addEventListener('click', () => {
+    menu.classList.remove('active');
+    });
+});
